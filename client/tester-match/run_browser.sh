a654212d369f45c67b058e79afb0d51c61b482d1
Pattern Miner <jbisa61@gmail.com>
2017-08-27 14:42:09 -0400

    HTTPS

---
+++ client/tester-match/run_browser.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/Applications/"Google Chrome.app"/Contents/MacOS/"Google Chrome" --incognito --ignore-certificate-errors \
+    https://localhost:8080/static/
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "net/http/pprof"
 	"github.com/patternMiner/bazelsauce/context"
 	"github.com/patternMiner/bazelsauce/handlers"
+	"crypto/tls"
 )
 
 func main() {
@@ -15,18 +16,39 @@ func main() {
 		fmt.Println(err)
 	}
 
-	http.HandleFunc("/info", handlers.InfoHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		w.Write([]byte("This is an example server.\n"))
+	})
+	mux.HandleFunc("/info", handlers.InfoHandler)
 
-	http.HandleFunc("/countries", handlers.CountriesHandler)
+	mux.HandleFunc("/countries", handlers.CountriesHandler)
 
-	http.HandleFunc("/devices", handlers.DevicesHandler)
+	mux.HandleFunc("/devices", handlers.DevicesHandler)
 
-	http.HandleFunc("/tester_match", handlers.MatchHandler)
+	mux.HandleFunc("/tester_match", handlers.MatchHandler)
 
 	fs := http.FileServer(http.Dir("client/tester-match/dist"))
-	http.Handle("/static/",
-		http.StripPrefix("/static/", fs))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	fmt.Println("Starting up the tester_match service on 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	fmt.Println("Starting up the tester_match https service on port 8080")
+	log.Fatal(srv.ListenAndServeTLS("localhost.pem", "server.key"))
 }
